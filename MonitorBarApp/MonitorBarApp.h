cb34634dbd6428b0ea4b4f2e2150a19943756820
Rostislav Egorov <rpegorov@icloud.com>
2025-12-08 16:23:30 +0300

    Initial Commit

---
+++ MonitorBarApp/MonitorBarApp.h
@@ -0,0 +1,14 @@
+//
+//  MonitorBarApp.h
+//  MonitorBarApp
+//
+//  Created by Ростислав Егоров on 08.12.2025.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface MonitorBarApp : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ MonitorBarApp/MonitorBarApp.m
@@ -0,0 +1,16 @@
+//
+//  MonitorBarApp.m
+//  MonitorBarApp
+//
+//  Created by Ростислав Егоров on 08.12.2025.
+//
+
+#import "MonitorBarApp.h"
+
+@implementation MonitorBarApp
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
